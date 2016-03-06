99acb7c5bdbfbc3b9cd5217734ac436c40eb4554
simonasdev <simonasgildutis@gmail.com>
2016-03-06 14:29:30 +0200

    Claiming

--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('ionicApp', ['ionic', 'restangular', 'ui.router'])
+angular.module('ionicApp', ['ionic', 'restangular', 'ui.router', 'ion-autocomplete'])
   .run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
       if(window.cordova && window.cordova.plugins.Keyboard) {
